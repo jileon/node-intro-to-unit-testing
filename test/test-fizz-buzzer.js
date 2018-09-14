83e87e70219e78151558c4e449359075c1548178
Janet Leon <leon.irenejanet@gmail.com>
2018-09-14 09:18:28 -0700

    conpleted fuzzbuzzer. need to refactor

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -7,14 +7,62 @@ describe('fizzBuzzer', function (){
 
   describe('if statement for fizz', function(){ 
     it('should return fizz if number is modulus 3', function(){
-      const normalCases=[3,6,9];
+      const normalCases=[3,6,9,12,18];
 
       normalCases.forEach((input)=>{
         expect(fizzBuzzer(input)).to.equal('fizz');
       });
-    // expect(fizzBuzzer(3)).to.equal('fizz');
     });
   });
+
+
+  describe('if statement for buzz', function(){ 
+    it('should return fizz if number is modulus 5', function(){
+      const normalCases=[5,10, 20, 25];
+      normalCases.forEach((input)=>{
+        expect(fizzBuzzer(input)).to.equal('buzz');
+      });
+    });
+  });
+
+  describe('if statement for fizz-buzz', function(){ 
+    it('should return fizz if number is modulus 15', function(){
+      const normalCases=[15,30, 45];
+      normalCases.forEach((input)=>{
+        expect(fizzBuzzer(input)).to.equal('fizz-buzz');
+      });
+    });
+  });
+
+
+  describe('if statement for num not % 3,5, or 15', function(){ 
+    it('should return number given as parameter', function(){
+      const normalCases=[1,2,4,7];
+      normalCases.forEach((input)=>{
+        expect(fizzBuzzer(input)).to.equal(input);
+      });
+    });
+  });
+
+  describe('if statement for num not % 3,5, or 15', function(){ 
+    it('should return number given as parameter', function(){
+      const normalCases=[1,2,4,7];
+      normalCases.forEach((input)=>{
+        expect(fizzBuzzer(input)).to.equal(input);
+      });
+    });
+  });
+
+  describe('if statement for NaN', function(){ 
+    it('should throw error for parameters that are NaN', function(){
+      const badCases=['1','cow','coffee'];
+      
+      badCases.forEach((input)=>{
+        expect( ()=>fizzBuzzer(input)).to.throw(Error);
+      });
+    });
+  });
+
 
 
 });
